853f97f7a6bbdffc9a8737303b6abb3a3de0c4c1
Aaron Miller <aaronmiller822028@gmail.com>
2019-07-08 22:31:52 -0700

    i could see this taking a week to really understand this. especially with the great new update of hooks!

--- src/App.js
+++ src/App.js
@@ -2,22 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {useSelector, useDispatch} from 'react-redux'
+
 function App() {
+
+  //callback function 
+  const counter = useSelector(state => state.counter )
+  const dispatch = useDispatch()
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Counter: {counter}</h1>
+        {/* just need to pass the action type */}
+        <button onClick={(()=> dispatch({type:"INCREMENT"}))}>INCREMENT</button>
+        <button onClick={(()=> dispatch({type:"DECREMENT"}))}>DECREMENT</button>
       </header>
     </div>
   );
